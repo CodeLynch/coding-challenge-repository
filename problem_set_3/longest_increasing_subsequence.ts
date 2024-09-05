1a439a064bb5ce312cd4899b3401ee4f0883964c
CodeLynch <torpedonialingnena@gmail.com>
2024-09-05 10:58:16 +0800

    solution for problem_set_3

---
+++ problem_set_3/longest_increasing_subsequence.ts
@@ -0,0 +1,18 @@
+
+
+
+function getLongestSub(nums:number[]): number{
+    
+    let countArr: number[] = Array(nums.length).fill(1);
+
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] <  nums[i] && countArr[i] == countArr[j])
+                countArr[i] = countArr[i] + 1;
+        }
+    }
+    return Math.max.apply(Math,countArr);
+}
+
+let inputArr = [10, 9, 2, 5, 3, 7, 101, 18]
+console.log(getLongestSub(inputArr));
